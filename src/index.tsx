af3d181f5d464493664bcc085647dce3b480597e
cielestrial <32034862+cielestrial@users.noreply.github.com>
2022-04-07 02:26:47 -0400

    Update boilerplate for use with my domain

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/donkey-car">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: `${__dirname}/src/index.tsx`,
   output: {
     path: `${__dirname}/build`,
-    publicPath: '/build/',
+    publicPath: '/donkey-car/build/',
     filename: 'bundle.js',
   },
 
@@ -45,7 +45,7 @@ module.exports = {
   // and test spa-github-pages redirect in dev
   devServer: {
     historyApiFallback: {
-      rewrites: [{ from: /\//, to: '/404.html' }],
+      rewrites: [{ from: /\/donkey-car\/[^?]/, to: '/404.html' }],
     },
   },
 };
