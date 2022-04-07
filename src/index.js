5fc1c94a17015aee89fdb4e2b6c6b95bb34d3e8e
cielestrial <32034862+cielestrial@users.noreply.github.com>
2022-04-07 07:26:24 -0400

    Removal of vitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./css/index.css";
 import App from "./pages/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
